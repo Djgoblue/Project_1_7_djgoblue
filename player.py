5cbb219bf28ccfd76f05f7f4a37658fdc7364656
Daniel Joo <dhjoo222@gmail.com>
2026-06-29 19:03:20 -0400

    Defined balance methods

--- player.py
+++ player.py
@@ -1,23 +1,35 @@
 class Player:
     def __init__(self, name, balance = 100):
-        self.name = name
-        self.balance = balance
-        self.hand = []
+        self.__name = name
+        self.__balance = balance
+        self.__hand = []
     
     #Hand methods
 
     #Add a card to the player's hand
     def draw(self, card):
-        self.hand.append(card)
+        self.__hand.append(card)
     
     #Clear the player's hand
     def clear(self):
-        self.hand = []
+        self.__hand = []
     
-    #Get player's hand
+    #Returns player's hand
     def get_hand(self):
-        return self.hand
+        return self.__hand
     
+    #Balance methods
+
+    #Return player's balance
+    def get_balance(self):
+        return self.__balance
     
+    #Add to player's balance
+    def add_balance(self, amount):
+        self.__balance += amount
     
+    #Subtract from player's balance
+    def subtract_balance(self, amount):
+        self.__balance -= amount
+
     
