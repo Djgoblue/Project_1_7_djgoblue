8686319cde7dfdef8394e4236ca8b5a02b339c02
Daniel Joo <dhjoo222@gmail.com>
2026-06-30 04:01:39 -0400

    Added welcome text, implemented player and cut card setup

--- blackjack.py
+++ blackjack.py
@@ -1,18 +1,19 @@
 from player import Player
 from deck import Shoe
 from dealer import Dealer
+import random as r
 
 class Game:
     def __init__(self):
         self.__dealer = Dealer()
-        self.__player = Player()
         self.__balance = 100
+        self.__shoe = Shoe()
 
     #Game Process
     def run(self):
         self.__welcome()
         while True:
-            choice = input.strip.lower()
+            choice = input().strip().lower()
             if choice == "p":
                 break
             elif choice == "e":
@@ -20,7 +21,8 @@ class Game:
             else:
                 print("\nInvalid choice. Type p to play or e to exit")
         
-        # self.__setup()
+        self.__setup()
+        self.__shoe.set_cut_card()
 
         # #While balance is non-zero keep playing
         # playing = True
@@ -39,16 +41,27 @@ class Game:
 
     def __welcome(self):
         print("\nWelcome to Python Blackjack!")
-        print("I assume you already know how to play or else you shouldn't be gambling.")
+        print("\nI assume you already know how to play or else you shouldn't be gambling.")
         print("Beat the dealer to 21 without going over.")
-        print("Blackjack pays 3:2 | Dealer stands on 17 | 8 decks in shoe | Choose where to cut shoe (Between 52 and 416)")
+        print("\nBlackjack pays 3:2 | Dealer stands on 17 | 8 decks in shoe | Choose where to cut shoe (Between 52 and 364)")
         print("Player starts with 100 chips | Minimum bet is 5")
-        print("Type p to play or e to exit")
+        print("\nType p to play or e to exit")
     
         
-    # def __setup(self):
-    #     print()
-    
+    def __setup(self):
+        print("\nWhat's your name?")
+        name = input().strip()
+        #Set name to player if nothing is entered
+        if not name:
+            name = "Player"
+        self.__player = Player(name)
+        print(f"\nWelcome, {self.__player.get_name()}!")
+        print("Choose where to place the cut card. (Between 52 and 364)")
+
+        
+
+            
+            
 
 #Run the game
 if __name__ == "__main__":
