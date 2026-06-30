6063286f32cd069a6298afea3a2e6e9e578c61ea
Daniel Joo <dhjoo222@gmail.com>
2026-06-30 03:37:55 -0400

    Moved cut card function from blackjack file to deck file

--- deck.py
+++ deck.py
@@ -1,4 +1,4 @@
-import random
+import random as r
 
 suits = ("Clubs", "Diamonds", "Hearts", "Spades")
 ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace")
@@ -14,17 +14,28 @@ class Shoe:
             for suit in suits:
                 for rank in ranks:
                     self.__shoe.append((rank, suit))
-        random.shuffle(self.__shoe)
+        r.shuffle(self.__shoe)
     
     #Draw card
     def draw(self):
         return self.__shoe.pop
     
     #Set cut card
-    def set_cut_card(self, depth):
-        if depth < 52 or depth > 416:
-            raise ValueError("Cut card depth must be between 52 and 416")
-        self.__cut_card_depth = depth
+    def set_cut_card(self, cut):
+        while True:
+            cut = input().strip()
+            if cut == "":
+                self.__cut_card_depth = r.randint(52, 364)
+                break
+            if not cut.isdigit():
+                print("Please enter a whole number.")
+                continue
+            cut = int(cut)
+            if cut < 52 or cut > 364:
+                print("Cut card depth must be between 52 and 364")
+                continue
+            self.__cut_card_depth = cut
+            break
         
     #Determine if cut card has been reached
     def cut_card_reached(self):
