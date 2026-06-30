f6911bbec799216b1d9863a10b91cad8b20c5111
Daniel Joo <dhjoo222@gmail.com>
2026-06-29 22:53:10 -0400

    Added draw card and cut card methods

--- deck.py
+++ deck.py
@@ -9,9 +9,23 @@ class Shoe:
         self.__build()
     
     #Builds a new shoe of cards
-    def __build(self):
+    def build(self):
         for _ in range(8):
             for suit in suits:
                 for rank in ranks:
                     self.__shoe.append((rank, suit))
         random.shuffle(self.__shoe)
+    
+    #Draw card
+    def draw(self):
+        return self.__shoe.pop
+    
+    #Set cut card
+    def set_cut_card(self, depth):
+        if depth < 52 or depth > 416:
+            raise ValueError("Cut card depth must be between 52 and 416")
+        self.__cut_card_depth = depth
+        
+    #Determine if cut card has been reached
+    def cut_card_reached(self):
+        return len(self.__shoe) <= self.__cut_card_depth
